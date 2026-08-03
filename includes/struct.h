b22135aed163f3c12469c6f71cbdab92d233d8b2
apolleux <axelpolleux@proton.me>
2026-08-03 20:39:52 +0200

    chore: developed hearder structs file - texture - game instance - integrated sdl2 and MacroLibx into struct.h by includes

--- includes/struct.h
+++ includes/struct.h
@@ -1,26 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/03 18:38:03 by apolleux          #+#    #+#             */
+/*   Updated: 2026/08/03 20:31:39 by apolleux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STRUCT_H
 # define STRUCT_H
 
-# include "stdbool.h"
+# include <stdbool.h>
+# include <SDL2/SDL_scancode.h>
+# include "mlx.h"
 
-typedef enum e_map
+typedef enum e_cell
 {
 	VOID,
 	WALL
-}			t_map;
+}	t_cell;
 
 typedef struct	s_map
 {
 	int				**content;
 	unsigned int	width;
 	unsigned int	height;
-}				t_map;
+}	t_map;
 
 typedef	struct	s_rules
 {
 	float	mov_speed;
 	float	rot_speed;
-}
+}	t_rules;
+
+typedef struct s_texture
+{
+	mlx_color	ground_color;
+	mlx_color	sky_color;
+	mlx_image	north_face;
+	mlx_image	south_face;
+	mlx_image	west_face;
+	mlx_image	east_face;
+}	t_texture;
 
 typedef struct	s_camera
 {
@@ -28,19 +52,22 @@ typedef struct	s_camera
 	float	pos_y;
 	float	dir_x;
 	float	dir_y;
-	float	plane_x;
-	float	plane_y;
-}				t_camera;
+	float	plan_x;
+	float	plan_y;
+}	t_camera;
 
 typedef struct	s_game
 {
-	t_map			map;
-	unsigned int	swidth;
-	unsigned int	sheight;
-	t_rules			rules;
+	bool			key_table[SDL_NUM_SCANCODES];
 	bool			running;
-	float			time;
+	float			new_time;
 	float			old_time;
-}				t_game;
+	unsigned int	screen_w;
+	unsigned int	screen_h;
+	t_map			map;
+	t_rules			rules;
+	t_texture		textures;
+	t_camera		camera;
+}	t_game;
 
 #endif
