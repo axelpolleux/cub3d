3aa11d2a3ece89cc14f968ba7a6434f433a7145e
apolleux <axelpolleux@proton.me>
2026-08-04 18:00:30 +0200

    fix: simplified and cleaned up my basic parser's files (ex: void, comments etc...)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lchamard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 13:06:41 by lchamard          #+#    #+#             */
-/*   Updated: 2026/08/04 16:17:01 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/04 17:52:52 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ int main(int argc, char *argv[])
 {
 	t_game	game;
 
-	(void)game;
-	// Parsing here, he define the map
-	
-	// Key hook here, he look the key, logic
 	if (!main_parser(argc, argv, &game))
 		return (1);
 	return (0);
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 13:38:08 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/04 16:17:13 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/04 17:53:09 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,6 @@ int	error(char *str)
 
 int	main_parser(int ac, char **av, t_game *game)
 {
-	int	height;
-	int	width;
-
-	(void)height;
-	(void)width;
-	(void)game;
 	if (ac > 2)
 		return (error("ヽ༼ຈʖ̯ຈ༽ﾉ\nWOW calm down !!\nI just need one argument"));
 	else if (ac < 2)
@@ -37,5 +31,6 @@ int	main_parser(int ac, char **av, t_game *game)
 	if (ft_strlen(av[1]) <= 4 || ft_strncmp(av[1] + (ft_strlen(av[1])
 				- 4), ".cub", 4) != 0)
 		return (error("x⸑x how can you miss it ?\nOnly .cub files"));
+
 	return (1);
 }
