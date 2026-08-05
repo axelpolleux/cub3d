49a6002560aea96ff8a9b3139a848906e6598c80
apolleux <axelpolleux@proton.me>
2026-08-05 19:15:24 +0200

    feat: integrated basic logic in fetch_path

--- src/parser/texture.c
+++ src/parser/texture.c
@@ -6,34 +6,26 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 14:32:06 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/05 17:09:06 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/05 19:10:11 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "parser.h"
-// #include "struct.h"
+#include "parser.h"
+#include "struct.h"
+
 
 char	*fetch_path(int fd, char *str)
 {
+	int		len;
+	char	*line;
+
+	len = ft_strlen(str);
+	while ((line = get_next_line(fd)))
+	{
+		if (ft_strlen(line) <= len)
+		{
+
+		}
+	}
 	return (0);
-}
-
-
-// int	set_path(t_game *game)
-// {
-// 	t_texture	texture;
-//
-// 	texture = game.textures;
-// }
-
-#include <fcntl.h>
-#include <stdio.h>
-
-int	main(void)
-{
-	int	fd = open("test", O_RDONLY);
-	printf("Nord: %s\n", fetch_path(fd, "NO"));
-	printf("Sud: %s\n", fetch_path(fd, "SO"));
-	printf("Est: %s\n", fetch_path(fd, "WE"));
-	printf("Ouest: %s\n", fetch_path(fd, "EA"));
 }
