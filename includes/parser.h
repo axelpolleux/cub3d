69890a25d7240922d68a9990a7d43c3ab19638fc
apolleux <axelpolleux@proton.me>
2026-08-05 18:26:14 +0200

    fix: added fetch_path in parser.h

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 13:43:10 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/05 10:39:13 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/05 17:24:32 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 # define PARSER_H
 
 # include "struct.h"
+# include "get_next_line.h"
 # include <fcntl.h>
 
-int	main_parser(int ac, char **av, t_game *game);
+int		main_parser(int ac, char **av, t_game *game);
+char	*fetch_path(int fd, char *str);
 
 #endif
