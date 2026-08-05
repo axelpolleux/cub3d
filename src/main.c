44444ed3b8084d6f5ef37a1b1476aa3460753f27
apolleux <axelpolleux@proton.me>
2026-08-05 11:19:05 +0200

    chore: changed *argv[] to **argv

--- src/main.c
+++ src/main.c
@@ -6,20 +6,18 @@
 /*   By: lchamard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 13:06:41 by lchamard          #+#    #+#             */
-/*   Updated: 2026/08/04 17:52:52 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/05 10:34:29 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "struct.h"
 
-int main(int argc, char *argv[])
+int	main(int argc, char **argv)
 {
 	t_game	game;
 
 	if (!main_parser(argc, argv, &game))
 		return (1);
 	return (0);
-
-
 }
