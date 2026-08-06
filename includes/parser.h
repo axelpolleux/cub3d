babfbaffbf71c14d7f76e83602f6c8097b1e8a4e
apolleux <axelpolleux@proton.me>
2026-08-06 17:57:31 +0200

    chore: added error function in parser.h

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 13:43:10 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/05 17:24:32 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/06 17:57:23 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@
 
 int		main_parser(int ac, char **av, t_game *game);
 char	*fetch_path(int fd, char *str);
+int		error(char *str);
 
 #endif
