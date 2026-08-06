704b8e8d309ad7e6dd89b512b36626c5db20a3f8
apolleux <axelpolleux@proton.me>
2026-08-06 17:58:13 +0200

    feat: added fetch_path function in texture parser part

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 13:38:08 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/05 18:49:26 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/06 17:01:06 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,6 @@ int	main_parser(int ac, char **av, t_game *game)
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 		return (error("I can't open your file\nskill issue !\n(ง •̀_•́)ง"));
-	printf("Nord: %s\n", fetch_path(fd, "NO"));
 	printf("Sud: %s\n", fetch_path(fd, "SO"));
-	printf("Est: %s\n", fetch_path(fd, "EA"));
-	printf("Ouest: %s\n", fetch_path(fd, "WE"));
 	return (1);
 }
--- src/parser/texture.c
+++ src/parser/texture.c
@@ -6,26 +6,37 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 14:32:06 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/05 19:10:11 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/06 17:57:55 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "parser.h"
 #include "struct.h"
 
-
 char	*fetch_path(int fd, char *str)
 {
-	int		len;
 	char	*line;
+	char	*res;
+	char	**s_line;
 
-	len = ft_strlen(str);
-	while ((line = get_next_line(fd)))
+	res = 0;
+	while (1)
 	{
-		if (ft_strlen(line) <= len)
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		s_line = ft_split(line, ' ');
+		if (!s_line)
+			return ((char *)(size_t)error("Malloc has failed\n"
+				"Everything is fine\n☉ ‿ ⚆"));
+		if (!s_line[0] && !s_line[1])
+			continue ;
+		if (ft_strncmp(str, s_line[0], ft_strlen(s_line[0])) == 0)
 		{
-
+			res = ft_calloc(ft_strlen(s_line[1]) + 1, sizeof(char));
+			ft_strlcpy(res, s_line[1], ft_strlen(s_line[1]) + 1);
 		}
 	}
-	return (0);
+	return (res);
 }
