1e1993838c6d2ca50817133c596d1eb2a03ce9fc
apolleux <axelpolleux@proton.me>
2026-08-09 18:56:37 +0200

    chore : initiated map and reorganised parser.h Add `set_map` function to handle game map initialization
    Introduced the `set_map` function in the parser to initialize and set up the game map based on file content. Updated related headers and added comments to improve code structure and clarity.

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 13:43:10 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/09 14:42:55 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/09 18:43:21 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,27 @@
 # include "get_next_line.h"
 # include <fcntl.h>
 
+// main part for parser to get started
 int		main_parser(int ac, char **av, t_game *game);
 int		error(char *str);
-char	*fetch_path(char *str, char **content);
+
+// initialising ready-game for renderer
+int		init_game(t_game *game, char **file_content);
+
+// checking game fundamentals
 int		check_file(char **av);
 int		check_folder(char **av);
+
+// checking and setting graphical parts
 int		set_textures(t_game *game, char **file_content);
-char	**fetch_content(int fd);
+int		set_map(t_game *game, char **file_content);
+
+// getting information from defined source
+char	*fetch_path(char *str, char **content);
 int		*fetch_colors(char *str, char **content);
-int		init_game(t_game *game, char **file_content);
+char	**fetch_content(int fd);
+
+// utils functions for my parser
 void	free_tab(char ***tab);
 
 #endif
