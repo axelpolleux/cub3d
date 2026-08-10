92bba2c14aa1143d37a6913061919fccc1e37335
apolleux <axelpolleux@proton.me>
2026-08-10 18:34:15 +0200

    chore: put back away init_game function unit tests for deployment in github pr

--- src/parser/game.c
+++ src/parser/game.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 18:54:23 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/10 17:41:45 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/08/10 18:30:11 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,9 @@
 
 int	init_game(t_game *game, char **file_content)
 {
-	// if (!set_textures(game, file_content)
-	// 	|| !set_map(game, file_content)
-	// 	|| !set_colors(game, file_content))
-	// 	return (0);
-
-	// test unitaires
-	// if (!set_textures(game, file_content))
-	// 	return (0);
-	if (!set_colors(game, file_content))
+	if (!set_textures(game, file_content)
+		|| !set_map(game, file_content)
+		|| !set_colors(game, file_content))
 		return (0);
-	// if (!set_map(game, file_content))
-	// 	return (0);
 	return (1);
 }
