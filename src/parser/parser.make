43d9a4c9a0fcb28a06ddf4857f9292d43b196c1a
apolleux <axelpolleux@proton.me>
2026-08-12 12:29:54 +0200

    feat: added validate.c

--- src/parser/parser.make
+++ src/parser/parser.make
@@ -6,6 +6,7 @@ SRC			+=	$(PARSER_PATH)/color.c		\
 				$(PARSER_PATH)/map.c		\
 				$(PARSER_PATH)/parser.c		\
 				$(PARSER_PATH)/player.c		\
-				$(PARSER_PATH)/texture.c
+				$(PARSER_PATH)/texture.c	\
+				$(PARSER_PATH)/player.c
 
 OUT_DIRS	+=	$(OUTPUT_DIR)/$(PARSER_PATH)
---
+++ src/parser/validate.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/11 16:59:06 by apolleux          #+#    #+#             */
+/*   Updated: 2026/08/12 12:28:25 by apolleux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+static	int	find_map_part(char **file_content)
+{
+	re
+}
+
+int	validate_keys(char **file_content, int *start)
+{
+	int		line_i;
+	char	**flags;
+
+	line_i = 0;
+	flags = (char *[]){"NO", "SO", "WE", "EA", "F", "C"};
+	return (1);
+}
