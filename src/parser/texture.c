cd3c6ab59a7b509c46011dda11e57a6e26ac686c
apolleux <axelpolleux@proton.me>
2026-08-12 11:36:42 +0200

    [NORMED] fix: normed fetch_path function for two functions (fetch_path and match_line)

--- src/parser/texture.c
+++ src/parser/texture.c
@@ -6,53 +6,49 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/09 14:03:07 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/10 19:34:38 by lchamard         ###   ########.fr       */
+/*   Updated: 2026/08/12 11:33:24 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "mlx.h"
 #include "parser.h"
 #include "struct.h"
+#include "mlx.h"
+
+static char	*match_line(char *str, char *line)
+{
+	char	*res;
+	char	**s_line;
+
+	res = NULL;
+	s_line = ft_split(line, ' ');
+	if (!s_line)
+		return (NULL);
+	if (s_line[0] && s_line[1] && ft_strlen(str) == ft_strlen(s_line[0])
+		&& ft_strncmp(str, s_line[0], ft_strlen(str)) == 0)
+	{
+		res = ft_calloc(ft_strlen(s_line[1]) + 1, sizeof(char));
+		if (res)
+			ft_strlcpy(res, s_line[1], ft_strlen(s_line[1]) + 1);
+	}
+	free_tab(&s_line);
+	return (res);
+}
 
 char	*fetch_path(char *str, char **content)
 {
 	int		i;
-	char	*line;
 	char	*res;
-	char	**s_line;
 
 	i = 0;
-	res = 0;
 	while (content[i])
 	{
-		line = content[i];
-		s_line = ft_split(line, ' ');
-		if (!s_line)
-			return (NULL);
-		if (!s_line[0])
-		{
-			free_tab(&s_line);
-			i++;
-			continue ;
-		}
-		if (s_line[1] && (ft_strlen(str) == ft_strlen(s_line[0]))
-			&& ft_strncmp(str, s_line[0], ft_strlen(str)) == 0)
-		{
-			res = ft_calloc(ft_strlen(s_line[1]) + 1, sizeof(char));
-			if (!res)
-			{
-				free_tab(&s_line);
-				break ;
-			}
-			ft_strlcpy(res, s_line[1], ft_strlen(s_line[1]) + 1);
-			free_tab(&s_line);
-			break ;
-		}
-		free_tab(&s_line);
+		res = match_line(str, content[i]);
+		if (res)
+			return (res);
 		i++;
 	}
-	return (res);
+	return (NULL);
 }
 
 t_image	load(char *path, mlx_context mlx)
@@ -63,8 +59,8 @@ t_image	load(char *path, mlx_context mlx)
 	fd = open(path, O_RDONLY);
 	if (fd > 0)
 	{
-		img.content = mlx_new_image_from_file(mlx, path, &img.width,
-				&img.height);
+		img.content = mlx_new_image_from_file(mlx, path,
+				&img.width, &img.height);
 		close(fd);
 	}
 	else
