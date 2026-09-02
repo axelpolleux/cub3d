fdb9cba18b828e1a684d5ba64029fb003ea4fcc4
apolleux <axelpolleux@proton.me>
2026-09-02 17:36:46 +0200

    fix: ensure `is_double` checks only before `start_map`, added null-checks and memory cleanup

--- src/parser/validate.c
+++ src/parser/validate.c
@@ -6,26 +6,46 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 16:59:06 by apolleux          #+#    #+#             */
-/*   Updated: 2026/09/01 18:07:57 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/09/02 17:21:19 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "libft.h"
 
-// static int	is_flag(char *str, char **flags)
+// int	not_this_char(char **map, char *chars)
+// {
+// 	int	i;
+// 	int	j;
+//
+// 	i = 0;
+// 	while (map[i])
+// 	{
+// 		j = 0;
+// 		while (map[i][j])
+// 		{
+// 			j++;
+// 			if (ft_strchr(chars, map[i][j]) == 0)
+// 				return (error("Character not accepted"));
+// 		}
+// 		i++;
+// 	}
+// 	return (1);
+// }
+
+// static int	is_map(char **file_content)
 // {
 // 	int	i;
 //
 // 	i = 0;
-// 	while (flags[i])
+// 	while (file_content[i])
 // 	{
-// 		if (ft_strlen(str) == ft_strlen(flags[i])
-// 			&& ft_strncmp(str, flags[i], ft_strlen(str)) == 0)
-// 			return (i);
+// 		printf("line %d\n[%s]", i, file_content[i]);
+// 		if (ft_strchr(file_content[i], '0') || ft_strchr(file_content[i], '1'))
+// 			return (i+1);
 // 		i++;
 // 	}
-// 	return (-1);
+// 	return (1);
 // }
 
 // static int	is_blank(char *line)
@@ -42,14 +62,7 @@
 // 	return (1);
 // }
 
-/*static int	is_map(char **file_content)
-{
-	// checker les lignes où il y a uniquement des '1' et/ou '0'
-	(void)file_content;
-	return (1);
-}*/
-
-static int	is_double(char **file_content)
+static int	is_double(char **file_content, int start_map)
 {
 	int		i;
 	int		j;
@@ -57,21 +70,30 @@ static int	is_double(char **file_content)
 	char	**sj_line;
 
 	i = 0;
-	while (file_content[i])
+	while (file_content[i] && i < start_map)
 	{
 		s_line = ft_split(file_content[i], ' ');
 		if (!s_line)
-			return (1);
+			return (0);
 		j = i + 1;
-		while (file_content[j])
+		while (file_content[j] && j < start_map)
 		{
 			sj_line = ft_split(file_content[j], ' ');
 			if (!sj_line)
+			{
+				free_tab(&s_line);
 				return (0);
+			}
 			if (!ft_strncmp(s_line[0], sj_line[0], ft_strlen(s_line[0])))
+			{
+				free_tab(&s_line);
+				free_tab(&sj_line);
 				return (0);
+			}
+			free_tab(&sj_line);
 			j++;
 		}
+		free_tab(&s_line);
 		i++;
 	}
 	return (1);
@@ -79,10 +101,9 @@ static int	is_double(char **file_content)
 
 int	check_keys(char **file_content, int *start)
 {
-	(void)start;
-	if (!is_double(file_content))
+	*start = 6;
+	if (!file_content[])
+	if (!is_double(file_content, *start))
 		return (error("A keyword is duplicated in your file\n(-_(-_-)_-)"));
-	/*if (!is_map(file_content))
-		return (error("Where is your map ?\nò_ô"));*/
 	return (1);
 }
