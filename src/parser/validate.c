6c05088ab1ccecafc31f21267455f664cc88217f
apolleux <axelpolleux@proton.me>
2026-09-02 18:26:46 +0200

    Refactor validation logic and improve error handling
    Removed unused functions from `validate.c` to simplify the code and focus on the required logic. Adjusted `check_keys` to handle missing map content more gracefully by adding a relevant error message.

--- src/parser/validate.c
+++ src/parser/validate.c
@@ -6,61 +6,12 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 16:59:06 by apolleux          #+#    #+#             */
-/*   Updated: 2026/09/02 18:05:45 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/09/02 18:26:34 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "libft.h"
-
-// int	not_this_char(char **map, char *chars)
-// {
-// 	int	i;
-// 	int	j;
-//
-// 	i = 0;
-// 	while (map[i])
-// 	{
-// 		j = 0;
-// 		while (map[i][j])
-// 		{
-// 			j++;
-// 			if (ft_strchr(chars, map[i][j]) == 0)
-// 				return (error("Character not accepted"));
-// 		}
-// 		i++;
-// 	}
-// 	return (1);
-// }
-
-// static int	is_map(char **file_content)
-// {
-// 	int	i;
-//
-// 	i = 0;
-// 	while (file_content[i])
-// 	{
-// 		printf("line %d\n[%s]", i, file_content[i]);
-// 		if (ft_strchr(file_content[i], '0') || ft_strchr(file_content[i], '1'))
-// 			return (i+1);
-// 		i++;
-// 	}
-// 	return (1);
-// }
-
-// static int	is_blank(char *line)
-// {
-// 	int	i;
-//
-// 	i = 0;
-// 	while (line[i])
-// 	{
-// 		if (line[i] != ' ' && line[i] != '\t')
-// 			return (0);
-// 		i++;
-// 	}
-// 	return (1);
-// }
 
 static int	is_double(char **file_content, int start_map)
 {
@@ -101,8 +52,10 @@ static int	is_double(char **file_content, int start_map)
 
 int	check_keys(char **file_content, int *start)
 {
-	*start = 6;
 	if (!is_double(file_content, *start))
 		return (error("A keyword is duplicated in your file\n(-_(-_-)_-)"));
+	*start = 6;
+	if (!file_content[*start])
+		return (error("Where is your map ?\n\¯\\(°_o)/¯"));
 	return (1);
 }
