9a3f1e160ea564cbae1d6c14626cfb4492f1f4a6
apolleux <axelpolleux@proton.me>
2026-09-03 11:27:14 +0200

    fix: check if the player is defined

--- src/parser/map.c
+++ src/parser/map.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/27 13:26:59 by apolleux          #+#    #+#             */
-/*   Updated: 2026/08/31 15:39:37 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/09/03 11:16:56 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ int	set_map(int start_map, t_game *game, char **file_content)
 			return (0);
 		y++;
 	}
-	if (!map_is_close(&game->map))
+	if (!player_is_define || !map_is_close(&game->map))
 	{
 		free(game->map.content);
 		return (0);
