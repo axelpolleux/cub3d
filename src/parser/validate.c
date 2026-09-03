de364728e8e4cf8f21332b2ded47a782d21ef657
apolleux <axelpolleux@proton.me>
2026-09-03 14:39:39 +0200

    fix: function is_double normed error fixed

--- src/parser/validate.c
+++ src/parser/validate.c
@@ -6,12 +6,28 @@
 /*   By: apolleux <apolleux@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 16:59:06 by apolleux          #+#    #+#             */
-/*   Updated: 2026/09/03 10:58:21 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/09/03 14:37:48 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "libft.h"
+
+static int	test_line(char ***sj_line, char ***s_line)
+{
+	if (!(*sj_line))
+	{
+		free_tab(s_line);
+		return (0);
+	}
+	if (!ft_strncmp((*s_line)[0], (*sj_line)[0], ft_strlen((*s_line)[0])))
+	{
+		free_tab(s_line);
+		free_tab(sj_line);
+		return (0);
+	}
+	return (1);
+}
 
 static int	is_double(char **file_content, int start_map)
 {
@@ -30,17 +46,8 @@ static int	is_double(char **file_content, int start_map)
 		while (file_content[j] && j < start_map)
 		{
 			sj_line = ft_split(file_content[j], ' ');
-			if (!sj_line)
-			{
-				free_tab(&s_line);
+			if (!test_line(&sj_line, &s_line))
 				return (0);
-			}
-			if (!ft_strncmp(s_line[0], sj_line[0], ft_strlen(s_line[0])))
-			{
-				free_tab(&s_line);
-				free_tab(&sj_line);
-				return (0);
-			}
 			free_tab(&sj_line);
 			j++;
 		}
